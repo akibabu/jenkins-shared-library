04c56e6c97c74be2838fd30213135a412df7c7a0
akibabu <3606722+akibabu@users.noreply.github.com>
2021-01-12 15:42:10 +0900

    Update scripted_pipeline.groovy

--- vars/scripted_pipeline.groovy
+++ vars/scripted_pipeline.groovy
@@ -8,6 +8,7 @@ def call(Map params) {
       println "Outer Stage 1"
       println params.get("param1")
       func()
+      ci.sharedFunc()
     }
     stage('Outer Stage 2') {
       stage('Inner Stage 1') {
