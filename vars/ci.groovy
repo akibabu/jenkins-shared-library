b08aefbfa52937679611afaee38f00049e0e0b77
akibabu <3606722+akibabu@users.noreply.github.com>
2021-01-12 13:24:34 +0900

    Create ci.groovy

---
+++ vars/ci.groovy
@@ -0,0 +1,30 @@
+def func() {
+  println "func()"
+}
+
+def sharedPipeline(Map params) {
+    pipeline {
+        agent any
+        options {
+            timeout(time: 1, unit: 'HOURS')
+        }
+        stages {
+            stage('Outer Stage 1') {
+                steps {
+                  println "Outer Stage 1"
+                  println params.get("param1")
+                }
+            }
+            stage('Outer Stage 2') {
+                stages {
+                    stage('Inner Stage 1') {
+                        steps {
+                            println "Inner Stage 1"
+                            println params.get("param2")
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
